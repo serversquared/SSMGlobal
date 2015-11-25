c1e8a64de7860c8314aaa700563bba68d3b9fc45
Niko Geil <code@serversquared.org>
2015-11-25 13:56:57 -0800

    Add help command.

--- main.py
+++ main.py
@@ -16,13 +16,25 @@ default_timeout = 10
 def cmd_quit(*args, **kwargs):
 	return True
 
+def cmd_help(*args, **kwargs):
+	client_from = args[0].getpeername()
+	data = 'Available commands:\r\n'
+	args[0].send(data.encode('ascii'))
+	for command_name in command_dispatch:
+		data = '\t{}\r\n'.format(command_name)
+		args[0].send(data.encode('ascii'))
+	args[1].put([json.dumps({'event' : 'send_data', 'data' : '(Help message)', 'client_from' : client_from})])
+
 def cmd_hw(*args, **kwargs):
+	client_from = args[0].getpeername()
 	for i in range(int(args[2])):
 		args[0].send('Hello, world!\r\n'.encode('ascii'))
+	args[1].put([json.dumps({'event' : 'send_data', 'data' : '(Hello world message)', 'client_from' : client_from})])
 # End command functions
 
 command_dispatch = {
 	'quit' : cmd_quit,
+	'help' : cmd_help,
 	'hw' : cmd_hw
 }
 
